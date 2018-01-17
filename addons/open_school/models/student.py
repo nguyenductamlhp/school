47ccd82db252afed2b7c6e500b4d79f4c820579d
huudanh2808 <huudanh2808@gmail.com>
2018-01-17 10:00:32 +0700

    update student and teacher inherit model res.users

--- addons/open_school/models/student.py
+++ addons/open_school/models/student.py
@@ -4,12 +4,13 @@ from odoo import fields, models
 
 class Student(models.Model):
 
-    _name = 'school.student' 
-
+    #_name = 'school.student' 
+    _inherit = 'res.users'
     name = fields.Char('Student name',required = True)
     student_code = fields.Char('Student code',required = True)
     score = fields.Float('Student score')
     age = fields.Integer('Student age')
+    is_student = fields.Boolean('Is student')
     class_id = fields.Many2one('school.class',string="Class")
     #diem -float
     #tuoi - int
